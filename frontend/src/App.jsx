b4b860b17854dc3b4d58edb125bae80f0077f1c0
yuriperformait <yuri.najar@performait.com>
2025-11-19 08:34:27 -0300

    run app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './assets/Components/Login/Login'
+import Home from './assets/Components/Home/Home'
 
 function App() {
 
   return (
-   <div className="App">
-     <Login/>
-   </div> 
-
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
